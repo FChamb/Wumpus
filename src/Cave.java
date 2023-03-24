f18410e8189e29293f2f01c71501b59b4a7766c4
Finnegan Z. Chamberlain <fc84@st-andrews.ac.uk>
2023-03-24 12:44:04 +0000

    Began adding board functional. Trying to spawn traps not in place of path.

--- src/Cave.java
+++ src/Cave.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Cave {
     int size;
@@ -6,6 +7,7 @@ public class Cave {
     int numOfSupBats;
     private final Room[][] cave;
     private final Player player;
+    private final Wumpus wumpus;
 
     public Cave(int size, int numOfPits, int numOfSupBats, Player player) {
         this.size = size;
@@ -13,6 +15,7 @@ public class Cave {
         this.numOfSupBats = numOfSupBats;
         this.cave = new Room[this.size][this.size];
         this.player = player;
+        this.wumpus = new Wumpus();
         generateCave();
     }
 
@@ -23,22 +26,14 @@ public class Cave {
             }
         }
         ArrayList<int[]> path = createPath();
-        for (int i = 0; i < this.size; i++) {
-            for (int j = 0; j < this.size; j++) {
-                for (int[] coord : path) {
-                    if (coord[0] == i && coord[1] == j) {
-                        this.cave[i][j] = new Room("+");
-                    }
-                }
-            }
-        }
-        //generateObstacles();
+        generateObstacles(path);
     }
 
     public ArrayList<int[]> createPath() {
         int[] start = getRandom();
         int length = this.size * 2;
         ArrayList<int[]> path = new ArrayList<>();
+        path.add(start);
         int lastRoom = -1;
         for (int i = 0; i < length; i++) {
             int nextRoom = (int) (Math.random() * 4);
@@ -51,100 +46,99 @@ public class Cave {
                 case 0:
                     lastRoom = 1;
                     if (x == 0) {
-                        x = this.size;
+                        x = this.size - 1;
                     } else {
                         x--;
                     }
                     path.add(new int[]{x, y});
-                    System.out.println(x + " " + y);
+                    start = new int[]{x, y};
                     break;
                 case 1:
                     lastRoom = 0;
-                    if (x == this.size) {
+                    if (x == this.size - 1) {
                         x = 0;
                     } else {
                         x++;
                     }
                     path.add(new int[]{x, y});
-                    System.out.println(x + " " + y);
+                    start = new int[]{x, y};
                     break;
                 case 2:
                     lastRoom = 3;
                     if (y == 0) {
-                        y = this.size;
+                        y = this.size - 1;
                     } else {
                         y--;
                     }
                     path.add(new int[]{x, y});
-                    System.out.println(x + " " + y);
+                    start = new int[]{x, y};
                     break;
                 case 3:
                     lastRoom = 2;
-                    if (y == this.size) {
+                    if (y == this.size - 1) {
                         y = 0;
                     } else {
                         y++;
                     }
                     path.add(new int[]{x, y});
-                    System.out.println(x + " " + y);
+                    start = new int[]{x, y};
                     break;
             }
         }
         return path;
     }
 
-    /**
-    public void generateObstacles() {
-        int numOfObs = this.numOfPits + this.numOfSupBats + 3;
+    public void generateObstacles(ArrayList<int[]> path) {
         int numPits = 0;
         int numBats = 0;
-        boolean treasure = true;
         boolean wumpus = true;
-        boolean player = true;
+        int x = path.get(0)[0];
+        int y = path.get(0)[1];
+        this.cave[x][y].setPlayerInRoom(true);
+        x = path.get(path.size() / 2)[0];
+        y = path.get(path.size() / 2)[1];
+        this.cave[x][y].setType("G");
+        x = path.get(path.size() - 1)[0];
+        y = path.get(path.size() - 1)[1];
+        this.cave[x][y].setType("X");
+        this.player.setCoords(x, y);
         for (int i = 0; i < this.size; i++) {
             for (int j = 0; j < this.size; j++) {
-                int choice = (int) (Math.random() * 5);
+                for (int[] coord : path) {
+                    if (coord[0] == i && coord[1] == j) {
+                        continue;
+                    }
+                }
+                int choice = (int) (Math.random() * 3);
                 switch (choice) {
                     case 0:
                         if (numPits < this.numOfPits) {
-                            this.cave[i][j].setAttribute(1);
+                            this.cave[i][j].setType("o");
                             numPits++;
                             break;
                         }
                     case 1:
                         if (numBats < this.numOfSupBats) {
-                            this.cave[i][j].setAttribute(2);
+                            this.cave[i][j].setType("w");
                             numBats++;
                             break;
                         }
                     case 2:
-                        if (treasure) {
-                            this.cave[i][j].setAttribute(3);
-                            treasure = false;
-                            break;
-                        }
-                    case 3:
                         if (wumpus) {
-                            this.cave[i][j].setAttribute(4);
+                            this.cave[i][j].setWumpusInRoom(true);
+                            this.wumpus.setCoords(i, j);
                             wumpus = false;
-                            break;
-                        }
-                    case 4:
-                        if (player) {
-                            this.cave[i][j].setPlayerInRoom(true);
-                            this.player.setCoords(i, j);
-                            player = false;
                             break;
                         }
                 }
             }
         }
-        if (numPits != this.numOfPits || numBats != this.numOfSupBats || treasure || wumpus || player) {
+        if (numPits != this.numOfPits || numBats != this.numOfSupBats || wumpus) {
+            //System.out.println("redo");
             clear();
-            generateObstacles();
+            generateObstacles(path);
         }
     }
-     */
 
     public int[] getRandom() {
         int x = (int) (Math.random() * this.size);
@@ -164,11 +158,13 @@ public class Cave {
     }
 
     public String toString() {
-        String output = "       Hunt the Wumpus \n";
+        String output = "Hunt the Wumpus \n";
         for (Room[] row : this.cave) {
             for (Room col : row) {
                 if (col.getPlayerInRoom()) {
                     output += this.player.toString();
+                } else if (col.getWumpusInRoom()) {
+                    output += this.wumpus.toString();
                 } else {
                     output += col.toString();
                 }
