5702d7451092a548ebed06c8e51a40b710bf932f
Finnegan Z. Chamberlain <fc84@st-andrews.ac.uk>
2023-04-11 12:13:43 +0100

    Commented all basic cave generation code.

--- src/game/Cave.java
+++ src/game/Cave.java
@@ -14,7 +14,23 @@ public class Cave {
     private final Wumpus wumpus;
     private ArrayList<String> validRooms;
 
+    /**
+     * Cave constructor takes seven parameters and creates a cave based on this given instructions. First
+     * a check sees if the provided values, in total, are greater than the number of available spaces. If
+     * so a new exception is thrown. Each of the private attributes in this class are then set to the
+     * specified rule. Lastly generateCave() is called which creates the layout for the 2D cave.
+     * @param xSize - height of the cave
+     * @param ySize - width of the cave
+     * @param numOfPits - number of pits in the cave
+     * @param numOfSupBats - number of super bats in the cave
+     * @param numWalls - number of walls in the cave
+     * @param numOfArtifacts - number of artifacts in the cave
+     * @param player - a player
+     */
     public Cave(int xSize, int ySize, int numOfPits, int numOfSupBats, int numWalls, int numOfArtifacts, Player player) {
+        if (numOfPits + numOfSupBats + numWalls + numOfArtifacts + xSize + ySize > xSize * ySize) {
+            throw new ArrayIndexOutOfBoundsException("Too many objects defined for the size of cave!");
+        }
         this.xSize = xSize;
         this.ySize = ySize;
         this.numOfPits = numOfPits;
@@ -24,26 +40,41 @@ public class Cave {
         this.player = player;
         this.wumpus = new Wumpus();
         this.numOfArtifacts = numOfArtifacts;
-        if (numOfPits + numOfSupBats + numWalls + numOfArtifacts + xSize + ySize > xSize * ySize) {
-            throw new ArrayIndexOutOfBoundsException("Too many objects defined for the size of cave!");
-        }
         this.validRooms = new ArrayList<>();
         generateCave();
     }
 
-    // getter for the layout
+    /**
+     * Getter for returning the layout of the cave
+     * @return - the 2D array of rooms which contain the cave
+     */
     public Room[][] getLayout(){
         return this.cave;
     }
-    // getter for the player
+
+
+    /**
+     * Getter for the player specified to this instance of cave
+     * @return - the current player
+     */
     public Player getPlayer(){
         return this.player;
     }
-    // getter for the wumpus
+
+    /**
+     * Getter for the Wumpus specified to this instance of cave
+     * @return - the current Wumpus
+     */
     public Wumpus getWumpus(){
         return this.wumpus;
     }
 
+    /**
+     * GenerateCave uses a nested for loops to fill the 2D array
+     * of the cave with new empty rooms. The arraylist of valid rooms
+     * is also filled with every object. Create path and generate
+     * obstacles are called after.
+     */
     public void generateCave() {
         for (int i = 0; i < this.xSize; i++) {
             for (int j = 0; j < this.ySize; j++) {
@@ -55,10 +86,19 @@ public class Cave {
         generateObstacles();
     }
 
+    /**
+     * Create path makes a valid path from the start to the end in which no objects can spawn, therefore making
+     * the game beatable. It first creates an arraylist of strings, path, which will contain the coordinates
+     * of each room on the valid path. Then the start of the path is found by calling getRandom. This room is
+     * removed from the validRooms list. A for loop cycles up the length of the path. In each instance of the loop
+     * the last room is stored and a while loop grabs the next direction ensuring that it is not equal to where the
+     * path has come from. A switch loop looks at the next direction to travel and sets the x/y coords properly,
+     * taking into account the torus layout of the cave. Finally, the player is set in the first room of the path, the
+     * treasure is set halfway through, and the exit is put at the end of the path.
+     */
     public void createPath() {
         ArrayList<String> path = new ArrayList<>();
         int[] start = getRandom();
-        this.validRooms.remove(start[0] + "x" + start[1]);
         int length = this.xSize + this.ySize;
         int lastRoom = -1;
         for (int i = 1; i <= length; i++) {
@@ -121,13 +161,22 @@ public class Cave {
         this.cave[x][y] = new Room("X");
     }
 
+    /**
+     * GenerateObstacles first creates a new attribute for every obstacle. Each of these are set to
+     * zero or true to allow the program to know how many have been placed. A total value is calculated
+     * from all the user defines numbers. A while loop cycles while the total is greater than zero. In this
+     * loop a new random location is chosen from the valid options. A random value is chosen to decide which
+     * object should be placed. And then a check ensures that the object has less placed than the total number
+     * required. If this is true the cave sets the appropriate type and that objects value is increased, and total
+     * is decreased. The valid room is also removed.
+     */
     public void generateObstacles() {
         int numPits = 0;
         int numBats = 0;
         boolean wumpus = true;
         int numOfItems = 0;
         int numWalls = 0;
-        int total = this.numOfPits + this.numOfSupBats + this.numWalls + this.numOfArtifacts + 1;
+        int total = this.numOfPits + this.numOfSupBats + this.numWalls + this.numOfArtifacts;
         int x; int y;
         while (total > 0) {
             int[] location = getRandom();
@@ -140,44 +189,49 @@ public class Cave {
                         numPits++;
                         total--;
                         this.validRooms.remove(x + "x" + y);
-                        break;
                     }
+                    break;
                 case 1:
                     if (numBats < this.numOfSupBats) {
                         this.cave[x][y] = new Room("w");
                         numBats++;
                         total--;
                         this.validRooms.remove(x + "x" + y);
-                        break;
                     }
+                    break;
                 case 2:
                     if (wumpus) {
                         this.wumpus.setCoords(x, y);
                         wumpus = false;
                         total--;
                         this.validRooms.remove(x + "x" + y);
-                        break;
                     }
+                    break;
                 case 3:
                     if (numOfItems < this.numOfArtifacts) {
                         this.cave[x][y].setArtifact(Artifact.getRandom());
                         numOfItems++;
                         total--;
                         this.validRooms.remove(x + "x" + y);
-                        break;
                     }
+                    break;
                 case 4:
                     if (numWalls < this.numWalls) {
                         this.cave[x][y] = new Room(" ");
                         numWalls++;
                         total--;
                         this.validRooms.remove(x + "x" + y);
-                        break;
                     }
+                    break;
             }
         }
     }
 
+    /**
+     * GetRandom find a random value from the private array list of valid rooms. Then
+     * the coordinates of that room are grabbed and returned in an array.
+     * @return - an array of ints containing the coords
+     */
     public int[] getRandom() {
         int i = (int) (Math.random() * this.validRooms.size());
         int x = Integer.parseInt(this.validRooms.get(i).split("x")[0]);
@@ -185,6 +239,10 @@ public class Cave {
         return new int[]{x, y};
     }
 
+    /**
+     * SetPlayer takes a random room in the cave, ensures it is not a wall, pit, or other super bat
+     * and sets the player to that new location.
+     */
     public void setPlayer() {
         int x = (int) (Math.random() * this.cave.length);
         int y = (int) (Math.random() * this.cave[1].length);
@@ -195,6 +253,11 @@ public class Cave {
         }
     }
 
+    /**
+     * toString() uses a nested for loop to fill a string values with the layout of the
+     * current game state. This value is returned.
+     * @return - a string with the layout of the board
+     */
     public String toString() {
         String output = "\n";
         for (int i = 0; i < this.cave.length; i++) {
@@ -212,6 +275,10 @@ public class Cave {
         return output;
     }
 
+    /**
+     * The main method, purely used for testing that this class functions properly.
+     * @param args - command line arguments, never used
+     */
     public static void main(String[] args) {
         Player test = new Player("Finnegan");
         Cave cave = new Cave(10, 10, 15, 5, 32, 5, test);
--- src/game/Player.java
+++ src/game/Player.java
@@ -9,24 +9,48 @@ public class Player {
     private boolean foundTreasure = false;
     private ArrayList<Artifact> inventory = new ArrayList<>();
 
+    /**
+     * Constructor that allows the user to choose player name. The icon is
+     * set to the first letter in the name
+     * @param name - a string for the look of the player
+     */
     public Player(String name) {
         this.icon = name.charAt(0);
     }
 
+    /**
+     * Constructor which takes a username, number of arrows, and if the treasure
+     * is found.
+     * @param name - a string with the name of the player
+     * @param numOfArrows - number of arrows a user has
+     * @param foundTreasure - if the treasure has been found
+     */
     public Player(String name, int numOfArrows, boolean foundTreasure) {
         this(name);
         this.numOfArrows = numOfArrows;
         this.foundTreasure = foundTreasure;
     }
 
+    /**
+     * Add arrow increases the arrow count by one.
+     */
     public void addArrow() {
         this.numOfArrows++;
     }
 
+    /**
+     * Set arrows sets the number of arrows for a player.
+     * @param arrows
+     */
     public void setArrows(int arrows){
         this.numOfArrows = arrows;
     }
 
+    /**
+     * Use arrow decreases the number of arrows by one and returns true is the user
+     * has arrows left over, false otherwise.
+     * @return
+     */
     public boolean useArrow() {
         if (this.numOfArrows > 0) {
             this.numOfArrows--;
@@ -35,34 +59,66 @@ public class Player {
         return false;
     }
 
+    /**
+     * Getter which returns the number of arrows a player has.
+     * @return - an int with the number of arrows
+     */
     public int getNumOfArrows() {
         return this.numOfArrows;
     }
 
+    /**
+     * Sets the coordinates of the player with a given height and width.
+     * @param x - the height of the player
+     * @param y - the width of the player
+     */
     public void setCoords(int x, int y) {
         this.coords = new int[]{x, y};
     }
 
+    /**
+     * Getter that returns the coords of the player
+     * @return - an int array with the players coords
+     */
     public int[] getCoords() {
         return this.coords;
     }
 
+    /**
+     * Getter which returns if the treasure has been found.
+     * @return - boolean for treasure found
+     */
     public boolean hadFoundTreasure(){
         return foundTreasure;
     }
 
+    /**
+     * Setter which sets the foundTreasure to true.
+     */
     public void findTreasure(){
         this.foundTreasure = true;
     }
 
+    /**
+     * Getter which returns the array list of artifacts the player has.
+     * @return - array list of artifacts
+     */
     public ArrayList<Artifact> getInventory() {
         return inventory;
     }
 
+    /**
+     * Add item add an artifact to the players inventory.
+     * @param artifact -  a given artifact
+     */
     public void addItem(Artifact artifact) {
         this.inventory.add(artifact);
     }
 
+    /**
+     * Sets the players name.
+     * @param name - a given name for a user
+     */
     public void setName(String name) {
         this.icon = name.charAt(0);
     }
@@ -75,6 +131,10 @@ public class Player {
         }
     }
 
+    /**
+     * Puts the player into a string and returns.
+     * @return
+     */
     public String toString() {
         return " " + this.icon + " ";
     }
